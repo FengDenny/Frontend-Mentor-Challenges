3ad7609d5a935ddd9171df2fb7b35e307876c941
Denny Feng <fengdennyy@gmail.com>
2024-05-20 11:30:23 -0700

    Created mobile menu

---
+++ Landing Page 04/Component/Menu.js
@@ -0,0 +1,19 @@
+const openMenuButton = document.getElementById("open-menu")
+const closeMenuButton = document.getElementById("close-menu")
+const menuOpened = document.getElementById("opened-menu")
+const overlay = document.getElementById("overlay")
+
+function toggleMenu(show){
+ const expandedOpenMenu = openMenuButton.getAttribute("aria-expanded") === "true" || false
+ const expandedCloseMenu = closeMenuButton.getAttribute("aria-expanded") === "true" || false
+ menuOpened.classList.toggle("show", show)
+ menuOpened.classList.toggle("not-show", !show)
+ overlay.classList.toggle("show", show)
+ overlay.classList.toggle("not-show", !show)
+ openMenuButton.setAttribute("aria-expanded", !expandedOpenMenu)
+ closeMenuButton.setAttribute("aria-expanded", !expandedCloseMenu)
+}
+
+openMenuButton.addEventListener("click", () => toggleMenu(true))
+closeMenuButton.addEventListener("click", () => toggleMenu(false))
+overlay.addEventListener("click", () => toggleMenu(false))
