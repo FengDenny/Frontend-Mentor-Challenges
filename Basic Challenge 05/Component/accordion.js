601d5c7f588cf8f92e6eb8a34c52a3c764da0735
Denny Feng <fengdennyy@gmail.com>
2024-05-22 14:27:29 -0700

    Created accordion functionality

--- Basic Challenge 05/Component/accordion.js
+++ Basic Challenge 05/Component/accordion.js
@@ -1,1 +1,23 @@
-console.log("Testing")
+function toggleAccordion() {
+  const accordionContainer = document.querySelector(".accordion");
+  accordionContainer.addEventListener("click", (event) => {
+    const button = event.target.closest(
+      'button[data-role="accordion-toggler"]'
+    );
+    if (button) {
+      const target = button.getAttribute("data-target");
+      const accordion = document.querySelector(
+        `article[data-role="accordion-toggled"][data-target="${target}"]`
+      );
+
+      if(accordion){
+        accordion.classList.toggle("collapsed")
+        accordion.classList.toggle("expanded")
+        button.classList.toggle("expanded")
+      }
+
+    }
+  });
+}
+
+toggleAccordion();
