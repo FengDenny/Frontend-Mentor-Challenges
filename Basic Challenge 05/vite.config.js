bd8b81afc3841cf04ebdfa24e82ea6dfcf3bd755
Denny Feng <fengdennyy@gmail.com>
2024-05-23 12:11:56 -0700

    added build file config

---
+++ Basic Challenge 05/vite.config.js
@@ -0,0 +1,13 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import fs from 'fs-extra';
+
+export default defineConfig({
+  build: {
+    // Add a build hook to run after the build has finished
+    onEnd: () => {
+      // Use fs-extra to copy the assets folder to the dist directory
+      fs.copySync('data', 'dist/assets');
+    },
+  },
+});
