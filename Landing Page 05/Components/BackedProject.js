bddd12645afa84e85933eb46d18c37ac83c90c4b
Denny Feng <fengdennyy@gmail.com>
2024-05-29 14:06:27 -0700

    Close modal ready to go

--- Landing Page 05/Components/BackedProject.js
+++ Landing Page 05/Components/BackedProject.js
@@ -6,14 +6,27 @@ const backedProjectModalLogic = {
       if (!document.getElementById("open-modal")) {
         backedProjectModalUI.createModalContent();
         backedProjectModalUI.modalContainer.style.opacity = "1";
+        backedProjectModalUI.modalContainer.style.pointerEvents = "auto";
+        this.handleModalClose()
       }
     });
+  },
+  handleModalClose() {
+    const closeButton = document.getElementById("close-modal")
+    if (closeButton) {
+      closeButton.addEventListener("click", () => {
+        backedProjectModalUI.modalContainer.style.opacity= "0";
+        backedProjectModalUI.modalContainer.style.pointerEvents= "none";
+        while (backedProjectModalUI.modalContainer.firstChild) {
+          backedProjectModalUI.modalContainer.removeChild(backedProjectModalUI.modalContainer.firstChild);
+        }
+      });
+    }
   },
 };
 
 const backedProjectModalUI = {
   modalContainer: document.getElementById("modal-container"),
-
   createElement(tagName, attributes = {}, textContent = "") {
     const element = document.createElement(tagName);
     Object.keys(attributes).forEach((attribute) => {
@@ -22,6 +35,20 @@ const backedProjectModalUI = {
     element.textContent = textContent;
     return element;
   },
+
+  createSVGElementNS(qualifiedName, attributes = {}, textContent = "") {
+    const URI = "http://www.w3.org/2000/svg";
+    const nsElement = document.createElementNS(
+      URI,
+      qualifiedName
+    );
+    Object.keys(attributes).forEach((attribute) => {
+      nsElement.setAttribute(attribute, attributes[attribute]);
+    });
+    nsElement.textContent = textContent;
+    return nsElement;
+  },
+
   createHeadingH2(attributes) {
     return this.createElement("h2", attributes);
   },
@@ -41,6 +68,22 @@ const backedProjectModalUI = {
     modalParagrapgh.textContent =
       "Want to support us in bringing Mastercraft Bamboo Monitor Riser out in the world?";
 
+    const closeModalSVG = this.createSVGElementNS("svg", {
+      id: "close-modal",
+      class: "modal-close",
+      width: "15",
+      height: "15",
+    });
+
+    const path = this.createSVGElementNS("path", {
+      d: "M11.314 0l2.828 2.828L9.9 7.071l4.243 4.243-2.828 2.828L7.07 9.9l-4.243 4.243L0 11.314 4.242 7.07 0 2.828 2.828 0l4.243 4.242L11.314 0z",
+      fill: "#000",
+      ["fill-rule"]: "evenodd",
+      opacity: ".4",
+    });
+
+    closeModalSVG.appendChild(path);
+    modal.appendChild(closeModalSVG);
     modal.appendChild(modalHeading);
     modal.appendChild(modalParagrapgh);
 
@@ -48,4 +91,10 @@ const backedProjectModalUI = {
   },
 };
 
-backedProjectModalLogic.handleBackProjectClicked();
+const modal = {
+  init() {
+    backedProjectModalLogic.handleBackProjectClicked();
+  },
+};
+
+modal.init();
