b563f5910fc32f83235f00904207846242714147
Denny Feng <fengdennyy@gmail.com>
2024-05-31 20:16:11 -0700

    Added element helpers

--- Landing Page 05/Components/Helpers.js
+++ Landing Page 05/Components/Helpers.js
@@ -27,5 +27,60 @@ export const Helpers = {
     } else if (value > max) {
       numberInput.value = max;
     }
-  }
+  },
 };
+
+export const createElementsHelpers = {
+  createElement(tagName, attributes = {}, textContent = "") {
+    const element = document.createElement(tagName);
+    Object.keys(attributes).forEach((attribute) => {
+      if (attribute === "required" && attributes[attribute]) {
+        element.required = true;
+      } else {
+        element.setAttribute(attribute, attributes[attribute]);
+      }
+    });
+    element.textContent = textContent;
+    return element;
+  },
+  createSVGElementNS(qualifiedName, attributes = {}, textContent = "") {
+    const URI = "http://www.w3.org/2000/svg";
+    const nsElement = document.createElementNS(URI, qualifiedName);
+    Object.keys(attributes).forEach((attribute) => {
+      nsElement.setAttribute(attribute, attributes[attribute]);
+    });
+    nsElement.textContent = textContent;
+    return nsElement;
+  },
+
+  createDiv(attributes, textContent) {
+    return this.createElement("div", attributes, textContent);
+  },
+  createArticle(attributes, textContent) {
+    return this.createElement("article", attributes, textContent);
+  },
+  createHeadingH2(attributes, textContent) {
+    return this.createElement("h2", attributes, textContent);
+  },
+  createParagraph(attributes, textContent) {
+    return this.createElement("p", attributes, textContent);
+  },
+  createInputType(attribute, textContent) {
+    return this.createElement("input", attribute, textContent);
+  },
+  createLabel(attribute, textContent) {
+    return this.createElement("label", attribute, textContent);
+  },
+  createSpan(attribute, textContent) {
+    return this.createElement("span", attribute, textContent);
+  },
+  createCTA(attribute, textContent) {
+    return this.createElement("button", attribute, textContent);
+  },
+  createProgressbar(attribute, textContent) {
+    return this.createElement("progress", attribute, textContent);
+  }
+}
+
+
+
