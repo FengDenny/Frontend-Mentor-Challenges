792fdf8bd84cba437df527e3226b49c036315d3b
Denny Feng <fengdennyy@gmail.com>
2024-06-02 15:45:43 -0700

    Updated bookmark toggle span text / active state

--- Landing Page 05/Components/Bookmark.js
+++ Landing Page 05/Components/Bookmark.js
@@ -5,6 +5,8 @@ const bookmarkLogic = {
   storedData: LocalStorage.checkLocalStorageData("bookmarked"),
   bookmarkedCircle: document.querySelector("#bookmark svg circle"),
   bookmarkedPath: document.querySelector("#bookmark svg path"),
+  bookmarkSpan: document.querySelector("span[data-target='bookmark']"),
+
 
   handleBookmarkClicked() {
     bookmarkUI.updateBookmarkClicked(this.storedData);
@@ -26,9 +28,13 @@ const bookmarkUI = {
     if (!storedData) {
       bookmarkLogic.bookmarkedCircle.setAttribute("fill", "#2F2F2F");
       bookmarkLogic.bookmarkedPath.setAttribute("fill", "#B1B1B1");
+      bookmarkLogic.bookmarkSpan.textContent = "Bookmark"
+      bookmarkLogic.bookmarkSpan.style.color = ""
     } else {
       bookmarkLogic.bookmarkedCircle.setAttribute("fill", "hsl(176, 72%, 28%)");
       bookmarkLogic.bookmarkedPath.setAttribute("fill", "hsl(0, 0%, 98%)");
+      bookmarkLogic.bookmarkSpan.textContent = "Bookmarked"
+      bookmarkLogic.bookmarkSpan.style.color = "hsl(176, 72%, 28%)"
     }
   }
 };
