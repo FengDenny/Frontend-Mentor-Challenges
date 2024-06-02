e0db417c16531e0e197fd7779ddc224e0682eaa9
Denny Feng <fengdennyy@gmail.com>
2024-06-02 11:08:16 -0700

    Removed handleLocalStorageUpdates  , handleProgressNumberUpdatesfunctions

--- Landing Page 05/Components/Progress.js
+++ Landing Page 05/Components/Progress.js
@@ -13,13 +13,10 @@ export const progressLogic = {
     { id: "days-left", total: "56", description: "days left" },
   ],
 
-
   handleProgressData() {
     LocalStorage.handleInitialLocalStorage(this.progressData);
-    this.handleUpdateUI()
+    this.handleUpdateUI();
   },
-
-
 
   handleProgressNumberData() {
     this.progressData.forEach((item) => {
@@ -28,38 +25,20 @@ export const progressLogic = {
   },
 
   handleUpdateUI() {
-    this.progressSection.textContent = "";  // Clear the section
-    this.handleProgressNumberData();      // Re-render the numbers
-    this.handleProgressBarData();         // Re-render the progress bar
+    this.progressSection.textContent = ""; // Clear the section
+    this.handleProgressNumberData(); // Re-render the numbers
+    this.handleProgressBarData(); // Re-render the progress bar
   },
   handleProgressBarData() {
     const totalBacked = LocalStorage.checkLocalStorageData(
       "total-amount-backed"
     );
-    if(totalBacked){
+    if (totalBacked) {
       const totalBackedValue = totalBacked.split(",").join("");
       const progressValue = parseInt(totalBackedValue);
       progressUI.createProgressBar(100000, progressValue);
     }
   },
-  handleProgressNumberUpdates(totalAmount, totalBacker){
-    const totalAmountBacked = LocalStorage.updateLocalStorageData( "total-amount-backed", totalAmount)
-    const totalBackers = LocalStorage.updateLocalStorageData("total-backers", totalBacker)
-    return {totalAmountBacked, totalBackers}
-  },
-  handleLocalStorageUpdates(item){
-    // key: item.id, value: localStorage value
-    const localStorageKeys = {
-      "total-amount-backed": "total-amount-backed", 
-      "total-backers": "total-backers",
-    }
-    if(localStorageKeys[item.id]){
-      LocalStorage.updateLocalStorageData(localStorageKeys[item.id], item.total)
-    }
- 
-  }
-
-
 };
 
 const progressUI = {
@@ -67,7 +46,6 @@ const progressUI = {
     const article = createElementsHelpers.createArticle({ id: item.id });
     const headingText =
       LocalStorage.checkLocalStorageData(item.id) || item.total;
-
 
     const heading = createElementsHelpers.createHeadingH2({}, headingText);
     const span = createElementsHelpers.createSpan({}, item.description);
