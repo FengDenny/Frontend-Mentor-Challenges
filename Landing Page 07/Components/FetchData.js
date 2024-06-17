4fceb6cf5eb7a0b3728300d99e60083125cbc431
Denny Feng <fengdennyy@gmail.com>
2024-06-16 20:06:58 -0700

    Created fetch data function logic to fetch job listings

--- Landing Page 07/Components/FetchData.js
+++ Landing Page 07/Components/FetchData.js
@@ -1,2 +1,16 @@
 // FETCH JSON DATA
-console.log("JSON DATA FIRED")
+export const fetchDataLogic = {
+  async FetchData(file) {
+    try {
+      const response = await fetch(file);
+      if (!response.ok) {
+        throw new Error("Something was wrong fetching the json data");
+      }
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      console.error("There has been a problem with your fetch operation:", err);
+      throw err;
+    }
+  },
+};
