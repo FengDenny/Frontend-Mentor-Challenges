a94aefcaac02458baddec3f300ba9e031894c8ad
Denny Feng <fengdennyy@gmail.com>
2024-06-16 20:08:01 -0700

    Created fetched job listing logics  using  fetchDataLogic.FetchData(file)

--- Landing Page 07/Components/Listings.js
+++ Landing Page 07/Components/Listings.js
@@ -1,1 +1,21 @@
-console.log("JOB LISTINGS FIRED")
+import { fetchDataLogic } from "./FetchData";
+
+export const listingsLogic = {
+  listings: "./data/data.json",
+  async fetchListings() {
+    try {
+      const listingData = await fetchDataLogic.FetchData(this.listings);
+      console.log(listingData);
+    } catch (error) {
+      console.error(`Error fetching listing data: ${error}`);
+    }
+  },
+};
+
+const initListings = {
+  init() {
+    listingsLogic.fetchListings();
+  },
+};
+
+initListings.init();
