1a0185ba74b70a9dd2b432c46f612eb4137ebf55
Denny Feng <fengdennyy@gmail.com>
2024-06-24 23:32:46 -0700

    Filtered result data finished

--- Landing Page 07/Components/ListingFiltered.js
+++ Landing Page 07/Components/ListingFiltered.js
@@ -1,4 +1,5 @@
 import { LocalStorage } from "../Components/LocalStorage";
+import { listingsUI } from "./Listings";
 export const listingsFilteredLogic = {
   filterByRoleOrLevel(selectedFilters, job) {
     // If no filters are provided, consider it a match
@@ -24,26 +25,21 @@ export const listingsFilteredLogic = {
   },
   filterResult(data, selectedFilters) {
     /*
-    Check if selectedFilters is defined. 
-    If not, we return true, meaning the filter condition is considered met. 
-    This ensures that if a filter isn't provided, it doesn't affect the filtering process, which can cause "undefined" error.
+    Check if selectedFilters array length . 
+    If empty, we return true, meaning the filter condition is considered met. 
+      - This ensures that if a filter isn't provided, it doesn't affect the filtering process, which can cause "undefined" error.
+    Otherwise, if have item we filter
     */
     return data.filter((job) => {
-      const roleMatch = selectedFilters.role
-        ? this.filterByRoleOrLevel(selectedFilters.role, job.role)
-        : true;
-      const languagesMatch = selectedFilters.languages
-        ? this.filterByLangsOrTools(selectedFilters.languages, job.languages)
-        : true;
-      const toolsMatch = selectedFilters.tools
-        ? this.filterByLangsOrTools(selectedFilters.tools, job.tools)
-        : true;
-      const levelMatch = selectedFilters.level
-        ? this.filterByRoleOrLevel(selectedFilters.level, job.level)
-        : true;
+      const roleMatch = selectedFilters.role.length === 0 || this.filterByRoleOrLevel(selectedFilters.role, job.role);
+      const languagesMatch = selectedFilters.language.length === 0 || this.filterByLangsOrTools(selectedFilters.language, job.languages);
+      const toolsMatch = selectedFilters.tool.length === 0 || this.filterByLangsOrTools(selectedFilters.tool, job.tools);
+      const levelMatch = selectedFilters.level.length === 0 || this.filterByRoleOrLevel(selectedFilters.level, job.level);
 
       // Return true only if all filters match
-      return roleMatch && languagesMatch && levelMatch && toolsMatch;
+      return roleMatch && languagesMatch && toolsMatch && levelMatch;
+
+     
     });
   },
 
@@ -65,23 +61,32 @@ export const listingsFilteredLogic = {
       }
     }
   },
-  filterAndLog(data, selectedFilters){
+  filterAndLog(data, selectedFilters) {
     const filteredData = this.filterResult(data, selectedFilters);
     console.log(filteredData);
+
+    // // Clear existing listings using hasChildNodes and removeChild
+    while (listingsUI.main.hasChildNodes()) {
+      listingsUI.main.removeChild(listingsUI.main.firstChild);
+    }
+
+    filteredData.forEach((job) => {
+      if (job.featured === true && job.new === true) {
+        listingsUI.createListingCards(job);
+      } else if (job.new === true) {
+        listingsUI.createListingCards(job);
+      } else {
+        listingsUI.createListingCards(job);
+      }
+    });
+
+    this.handleFetchAttachEventListeners(data, selectedFilters)
   },
 
-  fetchListingsHTMLDataAndFilter(data) {
+  handleFetchAttachEventListeners(data, selectedFilters) {
     const listingCards = document.querySelectorAll(
       "article[data-id=listing-card]"
     );
-
-      // Initialize selected filters from local storage or defaults
-  const selectedFilters = LocalStorage.checkLocalStorageData("selectedFilters") || {
-    role: [],
-    level: [],
-    languages: [],
-    tools: [],
-  };
 
     listingCards.forEach((listingCard) => {
       const filtersContainer = listingCard.querySelector(
@@ -95,9 +100,22 @@ export const listingsFilteredLogic = {
           selectedFilters
         );
 
-        this.filterAndLog(data, selectedFilters)
+        this.filterAndLog(data, selectedFilters);
       });
     });
-    this.filterAndLog(data, selectedFilters)
+  },
+
+
+  fetchListingsHTMLDataAndFilter(data) {
+    // Initialize selected filters from local storage or defaults
+    const selectedFilters = LocalStorage.checkLocalStorageData(
+      "selectedFilters"
+    ) || {
+      role: [],
+      level: [],
+      language: [],
+      tool: [],
+    };
+    this.filterAndLog(data, selectedFilters);
   },
 };
--- Landing Page 07/Components/Listings.js
+++ Landing Page 07/Components/Listings.js
@@ -1,6 +1,6 @@
 import { fetchDataLogic } from "./FetchData";
 import { listingsFilteredLogic } from "./ListingFiltered";
-import { LocalStorage } from "../Components/LocalStorage";
+import { createElementsHelpers } from "./CreateElementsHelpers";
 
 export const listingsLogic = {
   listings: "./data/data.json",
@@ -26,11 +26,180 @@ export const listingsLogic = {
   },
 };
 
+export const listingsUI = {
+  main: document.getElementById("job-listings"),
+
+  createListingCards(job) {
+    const cardListingContent = this.createListingCardContent(job);
+    const cardListingFilterCTA = this.createListingFilterCTA(job);
+    const { featured, new: isNew, logo, company } = job;
+    const article = this.createArticle(featured && isNew, isNew);
+    const figure = this.createFigureWithImage(logo, company);
+    article.appendChild(figure);
+    article.appendChild(cardListingContent);
+    article.appendChild(cardListingFilterCTA);
+    this.main.appendChild(article);
+  },
+  createListingCardContent(job) {
+    const {
+      featured,
+      new: isNew,
+      company,
+      position,
+      postedAt,
+      contract,
+      location,
+    } = job;
+    const listingContentDiv = createElementsHelpers.createDiv({
+      class: "listing-content",
+    });
+
+    // Create header
+    const header = createElementsHelpers.createHeader({
+      class: "listing-header",
+    });
+    const headingH2 = createElementsHelpers.createHeadingH2({}, company);
+    const spanDataNew = createElementsHelpers.createSpan(
+      { class: "new-listing" },
+      `New!`
+    );
+    const spanDataFeatured = createElementsHelpers.createSpan(
+      { class: "new-featured" },
+      `Featured`
+    );
+    header.appendChild(headingH2);
+    if (isNew && featured) {
+      header.appendChild(spanDataNew);
+      header.appendChild(spanDataFeatured);
+    } else if (isNew) {
+      header.appendChild(spanDataNew);
+    }
+
+    // Create job section
+    const jobDiv = createElementsHelpers.createDiv({ class: "job" });
+    const jobTitle = createElementsHelpers.createHeadingH2({}, position);
+
+    const jobDescriptionDiv = createElementsHelpers.createDiv({
+      class: "job-description",
+    });
+    const jobPosted = createElementsHelpers.createSpan(
+      { "data-postedAt": postedAt, class: "job-posted" },
+      postedAt
+    );
+    const jobContract = createElementsHelpers.createSpan(
+      { "data-contract": contract, class: "job-contract" },
+      contract
+    );
+    const jobLocation = createElementsHelpers.createSpan(
+      { "data-location": location, class: "job-location" },
+      location
+    );
+
+    jobDescriptionDiv.appendChild(jobPosted);
+    jobDescriptionDiv.appendChild(jobContract);
+    jobDescriptionDiv.appendChild(jobLocation);
+
+    jobDiv.appendChild(jobTitle);
+    jobDiv.appendChild(jobDescriptionDiv);
+
+    listingContentDiv.appendChild(header);
+    listingContentDiv.appendChild(jobDiv);
+    return listingContentDiv;
+  },
+  createListingFilterCTA(job) {
+    const { role, level, tools, languages } = job;
+    const listingFilterCTADiv = createElementsHelpers.createDiv({
+      class: "listings-filters",
+      ["data-filters"]: "listings-filters",
+    });
+    const filterByRoleCTA = createElementsHelpers.createCTA(
+      {
+        ["aria-label"]: `Filter by ${role} role`,
+        ["data-role"]: role,
+      },
+      role
+    );
+    const filterByLevelCTA = createElementsHelpers.createCTA(
+      {
+        ["aria-label"]: `Filter by ${level} level`,
+        ["data-level"]: level,
+      },
+      level
+    );
+    let filterByToolCTA = null;
+    let filterByLanguageCTA = null;
+
+    listingFilterCTADiv.appendChild(filterByRoleCTA);
+    listingFilterCTADiv.appendChild(filterByLevelCTA);
+
+    if (tools) {
+      tools.forEach((tool) => {
+        filterByToolCTA = createElementsHelpers.createCTA(
+          {
+            ["aria-label"]: `Filter by ${tool} tool`,
+            ["data-tool"]: tool,
+          },
+          tool
+        );
+        listingFilterCTADiv.appendChild(filterByToolCTA);
+      });
+    }
+    
+    if (languages) {
+      languages.forEach((language) => {
+        filterByLanguageCTA = createElementsHelpers.createCTA(
+          {
+            ["aria-label"]: `Filter by ${language} language`,
+            ["data-language"]: language,
+          },
+          language
+        );
+        listingFilterCTADiv.appendChild(filterByLanguageCTA);
+      });
+    }
+    
+    return listingFilterCTADiv;
+  },
+
+  createArticle(isNewFeatured = false, isNew = false) {
+    let createArticle = null;
+    if (isNewFeatured) {
+      createArticle = createElementsHelpers.createArticle({
+        ["data-id"]: "listing-card",
+        ["data-new-featured"]: "true",
+      });
+    } else if (isNew) {
+      createArticle = createElementsHelpers.createArticle({
+        ["data-id"]: "listing-card",
+        ["data-new"]: "true",
+      });
+    } else {
+      createArticle = createElementsHelpers.createArticle({
+        ["data-id"]: "listing-card",
+      });
+    }
+
+    return createArticle;
+  },
+  createFigureWithImage(src, alt) {
+    const createFigure = createElementsHelpers.createFigure({
+      class: "listing-logo",
+    });
+
+    const createImg = createElementsHelpers.createImg({
+      src,
+      alt,
+    });
+
+    createFigure.appendChild(createImg);
+    return createFigure;
+  },
+};
+
 const initListings = {
   data: listingsLogic.fetchListingDataModified(),
   async init() {
     const dataModified = await this.data;
-    console.log("Data modified: ", dataModified);
     listingsFilteredLogic.fetchListingsHTMLDataAndFilter(dataModified);
   },
 };
