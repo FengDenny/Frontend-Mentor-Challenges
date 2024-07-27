310454dda01280f7dfe5cca10323d02708e88613
Denny Feng <fengdennyy@gmail.com>
2024-07-27 13:25:06 -0700

    Added replies route to "/api/user-comments"

--- Landing Page 08/server/server.js
+++ Landing Page 08/server/server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const commentRoutes = require("./routes/comments")
 const importDataRoute = require("./routes/initialData")
 const dummyAuthUser = require("./routes/dummyAuthUser")
+const repliesRoute = require("./routes/replies")
 
 const app = express()
 
 app.use(express.json()); // Middleware to parse JSON bodies
 
 app.use('/api/import-data', importDataRoute);
-app.use('/api/user-comments', commentRoutes);
+app.use('/api/user-comments',commentRoutes, repliesRoute);
 app.use('/api/auth-user', dummyAuthUser);
 
 
