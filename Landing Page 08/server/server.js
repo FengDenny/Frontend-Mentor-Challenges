93b8bf6597dfb760eb6b59707f8b28e685bfae53
Denny Feng <fengdennyy@gmail.com>
2024-08-06 15:17:25 -0700

    Added cors middleware to resolve CORS (Cross-Origin Resource Sharing) issues

--- Landing Page 08/server/server.js
+++ Landing Page 08/server/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const commentRoutes = require("./routes/comments")
 const importDataRoute = require("./routes/initialData")
 const dummyAuthUser = require("./routes/dummyAuthUser")
 const repliesRoute = require("./routes/replies")
 
 const app = express()
+
+// Use CORS middleware
+app.use(cors());
 
 app.use(express.json()); // Middleware to parse JSON bodies
 
@@ -13,7 +17,7 @@ app.use('/api/user-comments',commentRoutes, repliesRoute);
 app.use('/api/auth-user', dummyAuthUser);
 
 
-const PORT = process.env.port || 6000;
+const PORT = process.env.port || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
