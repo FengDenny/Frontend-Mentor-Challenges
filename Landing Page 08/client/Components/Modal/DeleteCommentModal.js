e1780e14024a50a4934195e42c2ffc8986396374
Denny Feng <fengdennyy@gmail.com>
2024-08-22 10:26:18 -0700

    Added container for modal button / updated addButton to add modal btn container

--- Landing Page 08/client/Components/Modal/DeleteCommentModal.js
+++ Landing Page 08/client/Components/Modal/DeleteCommentModal.js
@@ -4,7 +4,7 @@ import Buttons from "../Button/Buttons";
 function DeleteCommentModal() {
   Modal.call(
     this,
-    "Delete Comment",
+    "Delete comment",
     "Are you sure you want to delete this comment? This will remove the comment and can't be undone."
   );
 }
@@ -16,11 +16,18 @@ DeleteCommentModal.prototype.createDeleteCommentModal = function(){
     const modal = this.createModal()
     const buttons = new Buttons(true) 
 
+    const modalBtnContainer = document.createElement("div")
+    modalBtnContainer.className = "modal-btn-container"
+
+
     const cancelButton = buttons.createButtonWithText("No, Cancel", "cancel-btn", "cancel-delete")
     const deleteButton = buttons.createButtonWithText("Yes, Delete", "delete-btn", "continue-delete")
 
-    this.addButton(cancelButton)
-    this.addButton(deleteButton)
+    modalBtnContainer.appendChild(cancelButton)
+    modalBtnContainer.appendChild(deleteButton)
+
+    this.addButton(modalBtnContainer)
+
 
     return modal
 }
