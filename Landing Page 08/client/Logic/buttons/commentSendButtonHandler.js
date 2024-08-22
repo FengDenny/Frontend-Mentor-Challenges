57cb770b80edbbbd43cb126a077dba3e382a1e61
Denny Feng <fengdennyy@gmail.com>
2024-08-22 16:17:26 -0700

    Event delegation to send post request  for our comment form

---
+++ Landing Page 08/client/Logic/buttons/commentSendButtonHandler.js
@@ -0,0 +1,28 @@
+import { postNewComment } from "../../API/endpoints/postEndpoints";
+import {renderComments} from "../../API/render/renderComments";
+const writeComment = document.getElementById("auth-write");
+
+async function handleSendButtonClicked(event) {
+  const target = event.target;
+
+  if (target.matches("#send-comment")) {
+    event.preventDefault();
+    const commentTextArea = document.getElementById("add-comment");
+    const commentContent = commentTextArea.value.trim();
+
+    if (commentContent) {
+      try {
+        await postNewComment(commentContent);
+        commentTextArea.value = "";
+        await renderComments();
+      } catch (error) {
+        console.error("Error adding comment:", error);
+      }
+    } else {
+      console.error("Comment content is empty.");
+    }
+  }
+}
+
+// Attach a single event listener to the body for event delegation
+writeComment.addEventListener("click", handleSendButtonClicked);
