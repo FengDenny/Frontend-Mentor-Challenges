be56ecec0fdfb9d3eb0ecb29bc1cfcd5e7d3a44f
Denny Feng <fengdennyy@gmail.com>
2024-08-25 15:25:55 -0700

    Added user profile for replies

--- Landing Page 08/client/Components/Card/RepliesCard.js
+++ Landing Page 08/client/Components/Card/RepliesCard.js
@@ -1,7 +1,15 @@
 import { createCardElements } from "../../Helper/utils/createCardElements";
+import UserProfileCard from "./UserProfileCard";
 
 function RepliesCard(reply) {
   this.reply = reply;
+  this.userProfileCard = new UserProfileCard(
+    reply.user.image.png, 
+    reply.user.username,
+    reply.createdAt, 
+    reply.tag, 
+    reply.edited 
+  );
 }
 
 RepliesCard.prototype.createRepliesCardElement = async function () {
@@ -14,6 +22,10 @@ RepliesCard.prototype.createRepliesCardElement = async function () {
     replyingTo: this.reply.replyingTo
   });
   replyCard.className += " reply-card";
+   // Create and append the user profile section
+   const profileSection = this.userProfileCard.createProfileElements();
+   // Insert profile section at the top
+   replyCard.insertBefore(profileSection, replyCard.firstChild); 
 
   return replyCard;
 };
