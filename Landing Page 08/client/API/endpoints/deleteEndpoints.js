a978e2cf821925c0011343647a467851662f08f1
Denny Feng <fengdennyy@gmail.com>
2024-08-25 20:00:15 -0700

    Added DELETE request endpoint for replies

--- Landing Page 08/client/API/endpoints/deleteEndpoints.js
+++ Landing Page 08/client/API/endpoints/deleteEndpoints.js
@@ -11,5 +11,16 @@ async function deleteAuthUserComment(postID) {
       console.error("Error:", err);
     }
   }
+async function deleteAuthUserReply(username, commentID, postID) {
+    try {
+      const deleteComment = await api
+        .setEndpoint(`/user-comments/${username}/comments/${commentID}/replies/${postID}/delete`)
+        .delete();
+      
+      return deleteComment;
+    } catch (err) {
+      console.error("Error:", err);
+    }
+  }
 
-export {deleteAuthUserComment}
+export {deleteAuthUserComment, deleteAuthUserReply}
