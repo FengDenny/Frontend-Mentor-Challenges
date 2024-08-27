962729d40f5d428b786691518ebada525fb41554
Denny Feng <fengdennyy@gmail.com>
2024-08-27 14:22:30 -0700

    Crteated PATCH request to edit replies

--- Landing Page 08/client/API/endpoints/patchEndpoints.js
+++ Landing Page 08/client/API/endpoints/patchEndpoints.js
@@ -13,4 +13,20 @@ async function editComment(commentContent, postID){
       }
 }
 
-export {editComment}
+
+async function editReplies(username, commentID, postID, commentContent){
+
+  try{
+    const updateReply = await api.setEndpoint(`/user-comments/${username}/comments/${commentID}/replies/${postID}/update`)
+    .setData({content:commentContent})
+    .request("PATCH")
+    return updateReply
+  }catch(err){
+    console.error("Error", err)
+  }
+
+}
+
+export {editComment, editReplies
+  
+}
